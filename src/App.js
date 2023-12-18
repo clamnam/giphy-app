3f414c054d330b057e1f9146f1d97631ff09b11a
clamnam <clamnam@protonmail.com>
2023-12-18 17:27:36 +0000

    react gif

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// import React, { useEffect, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import GiphyViewer from './componenets/GiphyViewer';
+import './assets/app.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='mt-5'>
+      <Row>
+        <Col>
+          <GiphyViewer/>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
